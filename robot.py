e13abbbe2b0cd95dd98c27a1cb4f806b5dc56293
prescientje <james.kedwards@gmail.com>
2016-09-25 23:58:02 -0400

    add first python file

---
+++ robot.py
@@ -0,0 +1,9 @@
+'''
+This program will emulate a robot moving around on a square table.
+
+'''
+
+def initialize():
+    robot_x = 0 
+    robot_y = 0 
+    robot_f = 0 #0 = north, 1 = east, 2 = south, 3 = west
